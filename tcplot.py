9d04cca69fa5fe03ed6031db9783db5e9b6fcff2
kalassak akani lapik <flerovium292@gmail.com>
2020-02-12 23:52:40 -0700

    - removed lines from storms crossing the dateline causing clutter - added annotations to display storm name/code for storms meeting the TC criteria - fixed bug where color was determined based on TC status at endpoint rather than startpoint

--- tcplot.py
+++ tcplot.py
@@ -1,3 +1,4 @@
+import math
 import netCDF4
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, addcyclic
@@ -15,21 +16,33 @@ for line in k:
 	data = line.strip('\n').split(',')
 	ignoreflag = True
 	if len(data) == 1:
+		#set flags to skip plotting the first point of a track
 		lastlat = False
 		lastlon = False
+		curname = data[0]
+		annotated = False
 		pass
 	else:
-		curlat = data[1]
-		curlon = data[2]
+		curlat = float(data[1])
+		curlon = float(data[2])
 		curflag = data[5]
+		if lastlat != False and math.fabs(lastlon-curlon) > 180:
+			#skip plotting one track point if it crosses the dateline
+			lastlat = False
+			lastlon = False
 		if lastlat != False:
-			if curflag == 'False':
+			if lastflag == 'False':
 				c = '#ff0000'
 			else:
 				c = '#00ff00'
+				if annotated == False:
+					#annotate first point in the track meeting TC criteria
+					plt.annotate(curname, xy=(lastlon, lastlat), xytext=(3, -2), textcoords='offset points', color='k', zorder=1)
+					annotated = True
 			plt.plot([lastlon, curlon], [lastlat, curlat], color=c, linestyle='-', linewidth=1, zorder=0)
 		lastlat = curlat
 		lastlon = curlon
+		lastflag = curflag
 			
 plt.savefig("/home/kalassak/ps-capstone/tracks.png", bbox_inches='tight', pad_inches=0, dpi=100)
 
